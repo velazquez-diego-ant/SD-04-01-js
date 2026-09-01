8820386d8293ebdd3a8e036ef632de0c768fac2e
Diego Antonio <diegiymorita@gmail.com>
2026-09-01 11:57:43 -0600

    feat: Se termino la Tarea 4.

--- src/task4.js
+++ src/task4.js
@@ -2,7 +2,8 @@ const arr = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14]
 
 // Escribe tu codigo por debajo de esta linea
 
-
+console.log(arr.splice(5,5));
+console.log(arr.length) // 15
 
 // TEscribe tu codigo por encima de esta linea
 
