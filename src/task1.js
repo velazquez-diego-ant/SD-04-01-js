a2f81e324e3c6a1f401554d8fcaaa58e569d301e
Diego Antonio <diegiymorita@gmail.com>
2026-09-01 11:54:40 -0600

    feat: Se termino la tarea 1.

--- src/task1.js
+++ src/task1.js
@@ -1,8 +1,11 @@
-const arr = []
+const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 // Escribe tu codigo por debajo de esta linea
-
-
+/**
+ *  Deberá tener los números del 1 al 20 en orden y el array debera tener un length de 20
+ */
+console.log(arr.push); // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+console.log(arr.length); // 20
 
 // Escribe tu codigo por encima de esta linea
 
