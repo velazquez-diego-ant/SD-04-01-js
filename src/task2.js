e0c6f4929a2bad9026f45d3cc120f7f42c8ca3ee
Diego Antonio <diegiymorita@gmail.com>
2026-09-01 11:56:19 -0600

    feat: Se termino la tarea 2

--- src/task2.js
+++ src/task2.js
@@ -3,6 +3,11 @@ const arr = [16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
 // Escribe tu codigo por debajo de esta linea
 
 // Escribe tu codigo por encima de esta linea
+/**
+ * Deberá tener los números del 1 al 16 en orden ascendente y un length de 16
+ */
+console.log(arr.reverse()); // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+console.log(arr.length); // 16
 
 //No tocar esta funcion
 export function taskArr(){
